7a1b5ff8ea8244114846cb799a72aa00a209bea0
anisiqbal <anisiqbal484@gmail.com>
2020-04-07 16:16:31 +0500

    website full done

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var loaderArea = document.querySelector('.loader');
 
 setTimeout(function(){ 
-    // loaderArea.style.display = "none";
+    loaderArea.style.display = "none";
     loaderArea.classList.add('hide');
 }, 3000);
 
